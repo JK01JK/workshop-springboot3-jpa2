ece80234fd40dad94bb08ac198a5e7dc2f19b5ba
JK01JK <julianojk01@gmail.com>
2024-05-24 18:03:48 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 	//tipo da minha entidade é User e o Id dela é Long
 	
